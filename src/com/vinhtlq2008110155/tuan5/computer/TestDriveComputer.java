f196b71963e6b059e1a6e5b9f52f4d32fcb6b760
HnivQ <nhutpro458@gmail.com>
2021-10-14 07:27:03 +0700

    bo sung them computer

---
+++ src/com/vinhtlq2008110155/tuan5/computer/TestDriveComputer.java
@@ -0,0 +1,13 @@
+/*
+* create date Oct 14, 2021
+* create time: 7:23:17 AM
+* author: hinvq
+*/
+package com.vinhtlq2008110155.tuan5.computer;
+
+public class TestDriveComputer {
+	public static void main(String[] args) {
+			Computer laptopVinh = new Computer("Lenovo","12/2021","Windows 11",16,"Core i9 11900","20000000","36 thang");
+	laptopVinh.show();
+	}
+}
